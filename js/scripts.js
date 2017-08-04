ff6e47b8804280cc81dedbe854b21330287033d2
Wiktor <wiktorszczepaniak1@gmail.com>
2017-08-04 12:38:56 +0200

    Some cosmetic changes in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 
 var a = prompt('Enter the value of a');
 var	b = prompt('Enter the value of b');
+var mathExpression = ( a * a ) + ( 2 * a * b ) - ( b * b )
 
-
-if ( ( a * a ) + ( 2 * a * b ) - ( b * b ) > 0 ) {
-    alert ('Expression (a * a) + (2 * a * b) - (b * b) equals positive number');
+if ( mathExpression > 0 ) {
+    alert ( mathExpression + '  equals positive number');
 }
 
-else if ((a * a) + (2 * a * b) - (b * b) < 0) {
-    alert ('Expression (a * a) + (2 * a * b) - (b * b) equals negative number');
+else if ( mathExpression < 0 ) {
+    alert ( mathExpression + ' equals negative number');
 }
 
 else  {
-	alert ('Expression (a * a) + (2 * a * b) - (b * b) equals zero');
+	alert (mathExpression + ' equals zero');
 } 
+
+console.log('('+ a + ' * ' + a + ') + (2 * ' + a + ' * ' + b + ') - ('+ b +' * ' + b +') = ' + mathExpression  );	
